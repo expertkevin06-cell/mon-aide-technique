ab1081a73d16be4036aed4f94eda793f2c772761
expertkevin06-cell <expertkevin06@gmail.com>
2026-08-24 20:03:01 +0200

    Update service worker cache management and assets

--- sw.js
+++ sw.js
@@ -1,19 +1,44 @@
-const CACHE = 'atkevin-v1';
-const ASSETS = ['./', './index.html', './css/style.css',
-  './js/app.js', './js/db.js', './js/admin.js', './js/ai.js'];
+const CACHE_NAME = 'at-kevin-v5';
+
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/style.css',
+  '/js/db.js',
+  '/js/app.js',
+  '/js/admin.js',
+  '/js/ai.js',
+  '/icons/icon.svg',
+  '/icons/car.svg',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(hit =>
-      hit || fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
         return res;
       })
-    )
+      .catch(() => caches.match(e.request))
   );
 });
